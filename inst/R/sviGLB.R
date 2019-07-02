7ab83c1f58abc9cb240e9ff291becb63a887dfd6
cmarmstrong <omni.armstrong@gmail.com>
2019-07-01 23:14:15 -0500

    working on sviGLB

--- inst/R/sviGLB.R
+++ inst/R/sviGLB.R
@@ -1,13 +1,17 @@
 library(devtools)
 library(parallel)
 library(raster)
+library(reshape2)
+library(rgdal)
 library(rgeos)
 ## library(ripums)
-devtools::load_all('/data/ripums')
+## devtools::load_all('/data/ripums')
+devtools::load_all('C:\\Users\\omnia\\OneDrive\\Documents\\GitHub\\ripums')
 library(rnaturalearth)
 
 ## SETUP
-workdir <- file.path(.Platform $file.sep, 'home', 'cmarmstrong', 'Desktop') # Sys.getenv('WORKDIR')
+## workdir <- file.path(.Platform $file.sep, 'home', 'cmarmstrong', 'Desktop') # Sys.getenv('WORKDIR')
+workdir <- getwd()
 datadir <- file.path(workdir, 'armstrong18', 'inst')
 rdadir <- file.path(datadir, 'RData')
 tifdir <- file.path(datadir, 'tif', 'PHL') # c('PHL', 'ZAF')
@@ -19,6 +23,12 @@ hdi <- read.csv(file.path(csvdir, 'GDL-Sub-national-HDI-data.csv'))
 hdi <- melt(hdi, id.vars=names(hdi)[1:5], variable.name='year', value.name='hdi')
 hdi $year <- substr(hdi $year, 2, 5)
 spHdi <- readOGR(file.path(shpdir, 'GDL-SHDI-SHP-2.shp'))
+spHdi[which(spHdi $GDLCode=="#N/A"), ] <- NA
+spHdi $iso_code <- as.character(spHdi $iso_code)
+lgc <- with(spHdi @data, !is.na(GDLCode) & is.na(iso_code))
+spHdi[lgc, 'iso_code'] <- substr(spHdi[lgc, ] $GDLCode, 1, 3)
+spHdi $iso_code <- as.factor(spHdi $iso_code)
+spHdi <- spHdi[!is.null(spHdi $iso_code), ]
 
 srl <- sapply(unique(spHdi $GDLCode), function(i) spI <- gUnaryUnion(spHdi[spHdi $GDLCode==i, ]))
 SpatialPolygons(sapply(srl, slot, 'polygons'))
