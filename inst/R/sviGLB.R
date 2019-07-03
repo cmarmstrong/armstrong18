2fe09836a7372895c3a1e7d7f3330821122fade8
Chandler M. Armstrong <cmarmstrong@cerrrb-wn-t1396.erdc.dren.mil>
2019-07-03 09:43:49 -0400

    implemented resampling for sviGLB, next step kriging...

--- inst/R/sviGLB.R
+++ inst/R/sviGLB.R
@@ -7,15 +7,15 @@ library(rgeos)
 ## library(ripums)
 devtools::load_all('/data/ripums')
 ## devtools::load_all('C:\\Users\\omnia\\OneDrive\\Documents\\GitHub\\ripums')
-library(rnaturalearth)
 library(sp)
 
 ## SETUP
-## workdir <- file.path(.Platform $file.sep, 'home', 'cmarmstrong', 'Desktop') # Sys.getenv('WORKDIR')
+## Sys.getenv('WORKDIR')
+## workdir <- file.path(.Platform $file.sep, 'home', 'cmarmstrong', 'Desktop')
 workdir <- getwd()
 datadir <- file.path(workdir, 'armstrong18', 'inst')
 rdadir <- file.path(datadir, 'RData')
-tifdir <- file.path(datadir, 'tif', 'PHL') # c('PHL', 'ZAF')
+tifdir <- file.path(datadir, 'tif') # c('PHL', 'ZAF')
 shpdir <- file.path(datadir, 'shp')
 csvdir <- file.path(datadir, 'csv')
 
@@ -64,5 +64,42 @@ hdi10 <- hdi[hdi $year==2010, ]
 src <- merge(spHdi, hdi10, by.x='GDLCode', by.y='GDLCODE')
 
 ## get NL here...
+## nlPeriod <- '2015' # Rnightlights::nlRange('201301', '201312')
+## nlType <- 'VIIRS.Y' # 'VIIRS.M'
+## nlStats <- list('mean', na.rm=TRUE)
 
-## resample on demand
+## tileList <- Rnightlights:::getNlTiles(nlType='VIIRS.Y') $name
+## Rnightlights:::downloadNlTiles(nlType, nlPeriod, tileList) # imperative
+
+## resample
+## rLc <- raster(system.file('extdata', 'visnav.vrt', package='rvisnav'))
+## iLc <- c(6, 7, 8) # urban, ag-gen, ag-paddy
+## cl <- parallel::makeForkCluster(3)
+## lLc <- parallel::parLapply(cl, iLc, function(i) {
+##     r <- raster::mask(rLc, rLc!=i, maskvalue=1, updatevalue=0)
+##     r==i ## do this directly and skip mask?
+## })
+## parallel::stopCluster(cl)
+## rAg <- lLc[[2]] | lLc[[3]]
+## writeRaster(lLc[[1]], file.path(tifdir, 'urb.tif'))
+## writeRaster(rAg, file.path(tifdir, 'ag.tif'))
+
+## ag=file.path(tifdir, 'ag.tif'),
+## urb=file.path(tifdir, 'urb.tif'),
+ch <- c(lcover.tif=system.file('extdata', 'visnav.vrt', package='rvisnav'),
+        ## dem.tif=system.file('extdata', 'srtm.vrt', package='rsrtm'),
+        weiss2.tif=file.path(tifdir, 'weiss.tif'),
+        lscan2.tif=file.path(tifdir, 'lscan.tif'),
+        nlites.tif=file.path(tifdir, 'nlites.vrt'))
+
+te <- c(-180, -90, 180, 90)
+tr <- rep(0.00833333, 2)
+tap <- TRUE
+## mapply(gdalUtils, ...)
+cl <- parallel::makeForkCluster(length(ch))
+parSapply(cl, names(ch), function(name, ...) {
+    if(name=='lcover.tif') r <- 'near'
+    else r <- 'bilinear'
+    gdalUtils::gdalwarp(ch[name], file.path(tifdir, name), r=r, ...)
+}, te=te, tr=tr, tap=tap)
+parallel::stopCluster(cl)
